b331d71d91aed8383dff5c57318d96be7b3653d8
ray <153447122+rayxl69@users.noreply.github.com>
2024-06-01 22:34:02 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+function toggleSidebar() {
+  let nav = document.querySelector(".nav");
+  let main = document.querySelector(".lista");
+  nav.classList.toggle("sidebar");
+  main.classList.toggle("sidebar");
+}
+
+let cards = document.querySelectorAll(".cardzao");
+
+let soma = 0
+cards.forEach((el) => {
+  let x = 0;
+  el.addEventListener("click", () => {
+    if (x == 0) {
+      el.style.borderColor = "orange";
+      el.classList.add("soma");
+      x = 1
+    } else {
+      el.style.borderColor = "#ffd9a8";
+      if (el.classList.contains("soma")) {
+        el.classList.remove("soma");
+      }
+      x = 0
+    }
+  });
+})
+
+let btn = document.querySelector("#btn")
+
+btn.addEventListener("click", () => {
+  
+  cards.forEach((el) => {
+    if (el.classList.contains("soma")) {
+      soma += Number(el.getAttribute('data-value'))
+      el.classList.remove("soma");
+    }
+  })
+
+
+  cards.forEach((el) => {
+    el.style.borderColor = "#ffd9a8";
+  })
+  let res = document.querySelector(".exibirValor")
+  res.style.display = "block";
+  res.innerHTML = `O TOTAL A PAGAR SERÁ: R$${soma},00`
+  if (soma === 0) {
+    res.style.display = "none";
+  }
+  soma = 0
+})
