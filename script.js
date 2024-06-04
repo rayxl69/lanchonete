bb2ed6dca63af35779a53d020e0073f04cb83839
ray <153447122+rayxl69@users.noreply.github.com>
2024-06-04 14:43:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,12 @@ function abrirFechar() {
 
 function checkOrientation() {
     if (window.innerWidth > window.innerHeight) {
-        document.querySelector('.cards').style.gridTemplateColumns = 1 + 'fr';
+      document.querySelector('.cards').style.gridTemplateColumns = 1 + 'fr';
+      let imagens = document.querySelectorAll('cardzao img');
+      imagens.forEach((el) => {
+        el.style.height = 50 + "%";
+        el.style.width = 50 + "%";
+      })
     } else {
         document.querySelector('.cards').style.gridTemplateColumns = 'repeat('+2+', '+1+'fr';
     }
